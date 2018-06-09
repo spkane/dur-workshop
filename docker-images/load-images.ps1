16547e0257815d740b2bf66b7a18ca6d131bdee5
Sean P. Kane <skane@newrelic.com>
2018-06-09 09:03:35 -0700

    Update scripts

--- docker-images/load-images.ps1
+++ docker-images/load-images.ps1
@@ -1,19 +1,19 @@
 #!/usr/bin/env powershell
 
-docker image load jenkins-class-latest.tar
-docker image load adejonge-helloworld-latest.tar
-docker image load debian-latest.tar
-docker image load fedora-22.tar
-docker image load gogs-gogs-01119.tar
-docker image load mongo-32.tar
-docker image load node-571.tar
-docker image load postgres-963.tar
-docker image load redis-325.tar
-docker image load registry-261.tar
-docker image load rocketchat-hubot-rocketchat-latest.tar
-docker image load rocketchat-rocketchat-latest.tar
-docker image load spkane-mybot-latest.tar
-docker image load spkane-starwars-latest.tar
-docker image load spkane-train-os-latest.tar
-docker image load traefik-latest.tar
-docker image load quantum-game.tar
+docker image load -i jenkins-class-latest.tar
+docker image load -i adejonge-helloworld-latest.tar
+docker image load -i debian-latest.tar
+docker image load -i fedora-22.tar
+docker image load -i gogs-gogs-01119.tar
+docker image load -i mongo-32.tar
+docker image load -i node-571.tar
+docker image load -i postgres-963.tar
+docker image load -i redis-325.tar
+docker image load -i registry-261.tar
+docker image load -i rocketchat-hubot-rocketchat-latest.tar
+docker image load -i rocketchat-rocketchat-latest.tar
+docker image load -i spkane-mybot-latest.tar
+docker image load -i spkane-starwars-latest.tar
+docker image load -i spkane-train-os-latest.tar
+docker image load -i traefik-latest.tar
+docker image load -i quantum-game.tar
